2252343b199d38cdba4e9e1b2a2aaaca097222e8
Tim Falzone <tim@flzn.us>
2014-12-10 14:16:24 -0500

    initial

---
+++ mem_con_buffer.go
@@ -0,0 +1,160 @@
+package mem_constrained_buffer
+
+import (
+	"bytes"
+	"io"
+	"io/ioutil"
+	"mime/multipart"
+	"os"
+)
+
+const DEFAULT_MEMORY_SIZE int64 = 1 << 18
+const FILE_NAME_PREFIX = "mem-buf-"
+
+type MemoryConstrainedBuffer struct {
+	b             *bytes.Buffer
+	tmpfile       string
+	max           int64
+	size          int64
+	removeOnClose bool
+	file          multipart.File
+}
+
+func NewMemoryConstrainedBuffer(maxMemory int64, removeOnClose bool) *MemoryConstrainedBuffer {
+	return &MemoryConstrainedBuffer{
+		max:           maxMemory,
+		removeOnClose: removeOnClose,
+	}
+}
+
+func NewDefaultMemoryConstrainedBuffer() *MemoryConstrainedBuffer {
+	return NewMemoryConstrainedBuffer(DEFAULT_MEMORY_SIZE, true)
+}
+
+func (m *MemoryConstrainedBuffer) Write(p []byte) (int, error) {
+	n, err := m.ReadFrom(bytes.NewReader(p))
+	return int(n), err
+}
+
+func (m *MemoryConstrainedBuffer) ReadFrom(r io.Reader) (int64, error) {
+	var (
+		n   int64
+		err error
+		buf bytes.Buffer
+	)
+
+	for {
+		n, err = io.CopyN(&buf, r, m.max+1)
+		if err != nil && err != io.EOF {
+			return 0, err
+		}
+
+		m.size += n
+
+		if err == io.EOF {
+			err = nil
+			break
+		}
+
+		if n > m.max {
+			// too big, write to disk and flush buffer
+			file, err := ioutil.TempFile("", FILE_NAME_PREFIX)
+			if err != nil {
+				return 0, err
+			}
+			defer file.Close()
+			n, err = io.Copy(file, io.MultiReader(&buf, r))
+			if err != nil {
+				os.Remove(file.Name())
+				return 0, err
+			}
+			m.tmpfile = file.Name()
+			m.size = n
+			break
+
+		} else {
+			m.max -= n
+		}
+	}
+
+	if m.tmpfile == "" {
+		m.b = &buf
+	}
+
+	return n, err
+}
+
+func (m *MemoryConstrainedBuffer) Len() int64 {
+	return m.size
+}
+
+func (m *MemoryConstrainedBuffer) Remove() (err error) {
+	if m.file == nil && m.tmpfile == "" {
+		return nil
+	}
+	if m.file != nil {
+		err = m.file.Close()
+	}
+	if m.tmpfile != "" {
+		err = os.Remove(m.tmpfile)
+	}
+	return err
+}
+
+func (m *MemoryConstrainedBuffer) open() error {
+	if m.file != nil {
+		return nil
+	}
+	if b := m.b; b != nil {
+		m.file = &sectionReadCloser{io.NewSectionReader(bytes.NewReader(m.b.Bytes()), 0, int64(b.Len()))}
+		return nil
+	}
+	f, err := os.Open(m.tmpfile)
+	m.file = f
+	return err
+}
+
+func (m *MemoryConstrainedBuffer) Read(p []byte) (n int, err error) {
+	if m.file == nil {
+		if err := m.open(); err != nil {
+			return 0, err
+		}
+	}
+	return m.file.Read(p)
+}
+
+func (m *MemoryConstrainedBuffer) ReadAt(p []byte, off int64) (n int, err error) {
+	if m.file == nil {
+		if err := m.open(); err != nil {
+			return 0, err
+		}
+	}
+	return m.file.ReadAt(p, off)
+}
+
+func (m *MemoryConstrainedBuffer) Seek(offset int64, whence int) (int64, error) {
+	if m.file == nil {
+		if err := m.open(); err != nil {
+			return 0, err
+		}
+	}
+	return m.file.Seek(offset, whence)
+}
+
+func (m *MemoryConstrainedBuffer) Close() error {
+	if m.removeOnClose {
+		defer m.Remove()
+	}
+	if m.file == nil {
+		return nil
+	}
+	return m.file.Close()
+}
+
+type sectionReadCloser struct {
+	*io.SectionReader
+}
+
+func (rc sectionReadCloser) Close() error {
+	return nil
+}
---
+++ mem_con_buffer_test.go
@@ -0,0 +1,171 @@
+package mem_constrained_buffer
+
+import (
+	"os"
+	"strings"
+	"testing"
+)
+
+func TestBufferWrite(t *testing.T) {
+	var err error
+	var n int
+
+	buf := NewMemoryConstrainedBuffer(10, true)
+
+	n, err = buf.Write([]byte("1234567890abcdefghijklmnopqrstuvwxyz"))
+	if n != 36 {
+		t.Errorf("wrong read bytes: %d", n)
+	}
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = buf.Close()
+	if err != nil {
+		t.Error(err)
+	}
+
+	buf2 := NewMemoryConstrainedBuffer(40, true)
+
+	n, err = buf2.Write([]byte("1234567890abcdefghijklmnopqrstuvwxyz"))
+	if n != 36 {
+		t.Errorf("wrong read bytes: %d", n)
+	}
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = buf2.Close()
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestBufferReadFrom(t *testing.T) {
+	var err error
+	var n int64
+
+	buf := NewMemoryConstrainedBuffer(10, true)
+
+	n, err = buf.ReadFrom(strings.NewReader("1234567890abcdefghijklmnopqrstuvwxyz"))
+	if n != 36 {
+		t.Errorf("wrong read bytes: %d", n)
+	}
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = buf.Close()
+	if err != nil {
+		t.Error(err)
+	}
+
+	buf2 := NewMemoryConstrainedBuffer(40, true)
+
+	n, err = buf2.ReadFrom(strings.NewReader("1234567890abcdefghijklmnopqrstuvwxyz"))
+	if n != 36 {
+		t.Errorf("wrong read bytes: %d", n)
+	}
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = buf2.Close()
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestBufferLen(t *testing.T) {
+	buf := NewMemoryConstrainedBuffer(10, true)
+
+	_, err := buf.ReadFrom(strings.NewReader("1234567890abcdefghijklmnopqrstuvwxyz"))
+	if err != nil {
+		t.Error(err)
+	}
+
+	if buf.Len() != 36 {
+		t.Errorf("Len() is wrong: %d", buf.Len())
+	}
+
+	err = buf.Close()
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestBufferReadSmall(t *testing.T) {
+	str := "1234567890abcdefghijklmnopqrstuvwxyz"
+
+	buf := NewMemoryConstrainedBuffer(10, true)
+
+	_, err := buf.ReadFrom(strings.NewReader(str))
+	if err != nil {
+		t.Error(err)
+	}
+
+	b := make([]byte, 36)
+	n2, err := buf.Read(b)
+	if n2 != 36 {
+		t.Errorf("wrong read bytes: %d", n2)
+	}
+	if err != nil {
+		t.Error(err)
+	}
+
+	if string(b) != str {
+		t.Errorf("string does not match: %s", b)
+	}
+
+	err = buf.Close()
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestBufferReadLarge(t *testing.T) {
+	str := "1234567890abcdefghijklmnopqrstuvwxyz"
+
+	buf := NewMemoryConstrainedBuffer(100, true)
+
+	_, err := buf.ReadFrom(strings.NewReader(str))
+	if err != nil {
+		t.Error(err)
+	}
+
+	b := make([]byte, 36)
+	n2, err := buf.Read(b)
+	if n2 != 36 {
+		t.Errorf("wrong read bytes: %d", n2)
+	}
+	if err != nil {
+		t.Error(err)
+	}
+
+	if string(b) != str {
+		t.Errorf("string does not match: %s", b)
+	}
+
+	err = buf.Close()
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestBufferCloseTempCleanup(t *testing.T) {
+	buf := NewMemoryConstrainedBuffer(4, true)
+
+	_, err := buf.ReadFrom(strings.NewReader("12345"))
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = buf.Remove()
+	if err != nil {
+		t.Error(err)
+	}
+
+	if _, err := os.Stat(buf.tmpfile); err == nil {
+		t.Errorf("tmpfile not removed")
+	}
+}
